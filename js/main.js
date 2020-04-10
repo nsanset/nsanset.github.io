1f796c7e2496cd5ad3e1ef67f426df882df1ec8a
nsanset <60108119+nsanset@users.noreply.github.com>
2020-04-10 19:13:54 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -7,4 +7,40 @@ $(".navbar a").click(function(){
    scrollTop:$("#" + $(this).data('value')).offset().top
   },1000)
   
- })
+ });
+
+
+
+
+
+$(function(){
+ $("#phone").mask('+38(000) 000-0000');
+ $('#applyForm').submit(function(e){
+ 	e.preventDefault();
+ 	var studentName = $("#name").val();
+ 	var phone = $("#phone").val();
+ 	var email = $("#email").val();
+ 	var course = $('#applyForm').data('course');
+ 	Email.send({
+    SecureToken : "57dcf116-5f35-40e7-a808-31f31226f042",
+    To : 'nagyrnyak@gmail.com',
+    From : "nagyrnyak@gmail.com",
+    Subject : "test",
+    Body : "And this is the body"
+}).then(
+  message => alert("message-test")
+);
+ 	$("#modal").removeClass("show-my");
+ 	$("#thanks").addClass("show-my");
+ });
+ $(".showModal").click(function(e){
+ 	$('#modal').addClass("show-my");
+ 	$('#applyForm').data('course',$(this).attr('id'));
+ });
+$("#closeModal").click(function(e){
+	$("#modal").removeClass("show-my");
+});
+$("#closeThanks").click(function(e){
+	$("#thanks").removeClass("show-my");
+	});
+});
