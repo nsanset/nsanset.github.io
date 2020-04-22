220c54cde1641113e6ca8281db20c3f954af1b30
nsanset <60108119+nsanset@users.noreply.github.com>
2020-04-22 17:55:45 +0300

    Add files via upload

--- final/js/main.js
+++ final/js/main.js
@@ -81,14 +81,15 @@ $(document).ready(function()
 });
 
 
+
 $(document).ready(function(){
 $('.slider_mini').slick({
         dots: true,
         slidesToShow: 1,
         slidesToScroll: 1,
-        arrows: false
-        // autoplay: true,
-        // autoplaySpeed: 2000
+        arrows: false,
+        autoplay: true,
+        autoplaySpeed: 2000
       });
 });
 
@@ -99,8 +100,49 @@ $('.slider_big').slick({
         slidesToShow: 1,
         slidesToScroll: 1,
         arrows: true,
-        // autoplay: true,
-        // autoplaySpeed: 3000
+        autoplay: true,
+        autoplaySpeed: 3000
       });
 });
 
+
+
+
+wow = new WOW(
+                    {
+                    boxClass:     'wow',      
+                    animateClass: 'animated',
+                    offset:       0,          
+                    mobile:       false,      
+                    live:         true        
+                    }
+                    )
+                    wow.init();
+
+
+
+
+
+
+$(function(){
+ $("#phone").mask('+38(000) 000-0000');
+ $('#applyForm').submit(function(e){
+    e.preventDefault();
+    var studentName = $("#name").val();
+    var phone = $("#phone").val();
+    var email = $("#email").val();
+    var course = $('#applyForm').data('course');
+    $("#modal").removeClass("show-my");
+    $("#thanks").addClass("show-my");
+ });
+ $(".showModal").click(function(e){
+    $('#modal').addClass("show-my");
+    $('#applyForm').data('course',$(this).attr('id'));
+ });
+$("#closeModal").click(function(e){
+    $("#modal").removeClass("show-my");
+});
+$("#closeThanks").click(function(e){
+    $("#thanks").removeClass("show-my");
+    });
+});
